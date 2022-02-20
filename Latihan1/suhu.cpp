c5441225c078150e258fd00ce9abc40d965eae5b
Feri Febria Laksana <68647766+ngajar-feri@users.noreply.github.com>
2022-02-20 11:05:24 +0000

    Latihan1

---
+++ Latihan1/suhu.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "../input.hpp"
+#include <string>
+using namespace std;
+int main(){
+float celc, fahr, rea, kelv;
+cout<<"==============================================\n";
+cout<<"    PROGRAM KONVERSI SUHU DARI CELCIUS KE :   \n";
+cout<<"        FAHRENHEIT | REAMUR | KELVIN          \n";
+cout<<"==============================================\n";
+cout<<"Masukkan suhu dalam celcius = ";
+cin>>celc;
+/////
+fahr = (celc*1.8) + 32;
+rea  =  celc * 0.8;
+kelv = celc + 273.15;
+/////
+cout<<"=============================================\n";
+cout<<"              HASIL KONVERSI SUHU            \n";
+cout<<"=============================================\n";
+cout<<"FAHRENHEIT  =  "<<fahr<<endl;
+cout<<"REAMUR      =  "<<rea<<endl;
+cout<<"KELVIN      =  "<<kelv<<endl;
+cout<<"=============================================\n";
+cout<<"Enter 1x Again to Exit\n";
+getch();
+ 
+}
